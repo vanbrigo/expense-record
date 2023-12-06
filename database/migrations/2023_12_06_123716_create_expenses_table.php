87d21f67addd0b4ada09988d325b998197cf5f1f
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-06 13:42:42 +0100

    feat: create table expenses and add foreign keys

---
+++ database/migrations/2023_12_06_123716_create_expenses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->string('description');
+            $table->date('date');
+            $table->unsignedBigInteger('pay_method_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('pay_method_id')->references('id')->on('pay_method');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
