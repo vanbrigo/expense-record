0340287f1e74b8b35231f0ed696588ffb4c367ab
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-06 13:35:09 +0100

    feat: create table incomes

---
+++ database/migrations/2023_12_06_121117_create_incomes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->string('description');
+            $table->date('date');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('incomes');
+    }
+};
