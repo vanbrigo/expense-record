c26768eb1fa8667d96e431e4e65aa97077e74e7e
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-06 13:08:05 +0100

    feat: create table balances

---
+++ database/migrations/2023_12_06_114420_create_balances_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->decimal('income',10,2)->default(0);
+            $table->decimal('expenses',10,2)->default(0);
+            $table->decimal('balance',10,2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('balances');
+    }
+};
