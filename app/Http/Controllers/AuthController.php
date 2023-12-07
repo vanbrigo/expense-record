0ecadaa2c41b9d83672b215169569c1e34cba69c
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-07 13:09:40 +0100

    feat: add register and login functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Error;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,33 +15,15 @@ class AuthController extends Controller
     {
         try {
             
-            $validated = $request->validate([
+            $userValidated = $request->validate([
                 'nickname' => 'required|min:3|max:50',
                 'email' => 'required|unique:users|email',
-                'password' => 'required|min:8|max:12',
-                'avatar_url' => 'url:http,https'
+                'password' => 'required|min:6|max:12',
+                'avatar_url' => 'url:http,https|image|nullable|max:500'
             ]);
 
-            // if () {
-            //     return response()->json(
-            //         [
-            //             "success" => false,
-            //             "message" => "Error registering user",
-            //             // "error" => $validator->errors()
-            //         ],
-            //         Response::HTTP_BAD_REQUEST
-            //     );
-            // }
-
-            $password = $request->input('password');
-            $encryptedPassword = bcrypt($password);
-
-            $newUser = User::create(
-                [
-                    "nickname" => $request->input('nickname'),
-                    "email" => $request->input('email'),
-                    "password" => $encryptedPassword
-                ]
+            $newUser= User::create(
+                $userValidated
             );
 
             // enviar email de bienvenida
@@ -63,4 +47,70 @@ class AuthController extends Controller
             );
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $userValidated = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+            $email=$userValidated['email'];
+            $user = User::query()->where('email', $email)->first();
+    
+            if (!$user) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Email or password are invalid 1"
+                    ],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+
+            if (!Hash::check($userValidated['password'], $user->password)) {
+                throw new Error('Email or password are invalid 2');
+
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Email or password are invalid 2"
+                    ],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+
+            $token = $user->createToken('apiToken')->plainTextToken;
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "User Logged succesfully",
+                    "token" => $token,
+                    "data" => $user
+                ]
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            if($th->getMessage() === 'Email or password are invalid 2') {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Email or password are invalid 2"
+                    ],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error login user"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
+
+
