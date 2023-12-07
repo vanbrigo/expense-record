50e8bf05e4cd872d06ac0ca536c5725e1d8ddd29
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-07 09:13:16 +0100

    feat: add Income and Pay_method models

---
+++ app/Models/Expense.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    public function PayMethod(): BelongsTo
+    {
+        return $this->belongsTo(Pay_Method::class);
+    }
+}
