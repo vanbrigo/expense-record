b4ff19b985ce31c6c15f950f5f255f9b8d8c0ffb
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-07 19:27:59 +0100

    feat: add logout function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    //
+    public function logout(Request $request)
+    {
+        $accessToken = $request->bearerToken();
+        $token = PersonalAccessToken::findToken($accessToken);
+        
+        $token->delete();
+        
+        return response()->json(
+            [
+                "success" => true,
+                "message" => "Logout successfully"
+            ],
+            Response::HTTP_OK
+        );
+    }
 }
