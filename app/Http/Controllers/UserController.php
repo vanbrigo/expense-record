24041d28126ad5c97cc78531d36d98678ecf7403
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-07 19:38:20 +0100

    feat: add function to get user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+    public function profile(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+        return response()->json(
+            [
+                "success" => true,
+                "message" => "User profile",
+                "data" => $user
+            ],
+            Response::HTTP_OK
+        );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error getting user profile"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
     public function logout(Request $request)
     {
         $accessToken = $request->bearerToken();
