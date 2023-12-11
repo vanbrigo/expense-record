7a3dfbd235564ca7493551cec122bb7af1b31aed
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-11 19:26:24 +0100

    refactor: modify fields in categories table

--- database/migrations/2023_12_05_123853_create_categories_table.php
+++ database/migrations/2023_12_05_123853_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('type');
+            $table->text('icon_url');
             $table->timestamps();
         });
     }
